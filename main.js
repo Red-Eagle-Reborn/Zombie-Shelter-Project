600fab7ff3379a9d3b107c5216b5f7ae08226029
respectZ <respectZ@users.noreply.github.com>
2016-04-13 17:02:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -41,14 +41,43 @@ addWallHealthToRenderer(wallRenderer);
 
 //Variable[s]
 var wall=[];
+//Shelter Level:Health Shelter
+var shelterLevel=[[1,500],[2,750],[3,1050],[4,1500]];
+//1 easy,2 normal,3 hard,4 expert,5 master
+var currentLevel=1;
+var currentDifficulty=1;
+var Difficulty=[[1,3],[2,7],[3,10],[4,12],[5,30]];
+
+Block.defineBlock(236, "Easter Egg", ["piston_inner", 0], 2, false, 0);
+//Block.setShape(236, 0, 0, 0, 0, 0, 0);
+Block.setLightLevel(236, 0);
+Block.setRenderLayer(236, 1);
+Block.setLightOpacity(236, 1E-4);
 
 ModPE.setItem(898,"egg",0,"Wall Spawner Test",1);
 
 Player.addItemCreativeInv(898, 1, 0);
 
+print("If you in Creative Mode, it will switch to survival");
+
+var Shelter = {
+    LoadData: function() {
+        var checkSave=ModPE.loadData(Level.getWorldDir()+"shelter");
+        if(checkSave!=""||checkSave!=null||checkSave!=undefined) {
+            print("Save found, loading...")
+            currentLevel=checkSave;
+        }
+    }
+}
+
+function selectLevelHook() {
+    Shelter.LoadData();
+}
+
 function useItem(x,y,z,i,b,s) {
     if(i==898) {
         var ent=Level.spawnMob(x+0.5,y+1,z+0.5,15,"shelter/shelter.png");
+        Entity.setMaxHealth(ent,500);
         Entity.setHealth(ent,500);
         Entity.setRenderType(ent,wallRenderer.renderType);
         wall.push([ent,Entity.getX(ent),Entity.getY(ent),Entity.getZ(ent)]);
@@ -59,7 +88,7 @@ function modTick() {
     if(wall.length!=0) {
         for(var i=0;i<wall.length;i++) {
             Entity.setPosition(wall[i][0],wall[i][1],wall[i][2],wall[i][3]);
-            Entity.setRot(wall[i][0],0,0)
+            Entity.setRot(wall[i][0],90,0);
             if(Player.getPointedEntity()==wall[i][0]) {
                 ModPE.showTipMessage("Wall Health : "+Entity.getHealth(wall[i][0]));
             }
@@ -70,24 +99,28 @@ function modTick() {
 function attackHook(a,v) {
     if(wall.length!=0) {
         for(var t=0;t<wall.length;t++) {
-            if(a==getPlayerEnt()&&v==wall[t][0]) {
-                preventDefault();
+            if(v==wall[t][0]) {
+                if(a==getPlayerEnt()) {
+                    preventDefault();
+                } else if(a!=getPlayerEnt()) {
+                    preventDefault();
+                    Entity.setHealth(wall[t][0],currentLevel*3);
+                }
             }
         }
     }
 }
 
 function entityAddedHook(e) {
-    if(Entity.getMobSkin(e)!="shelter/shelter.png"||Entity.getMobSkin(e)!="mob/zombie.png") {
-        Entity.remove(e)
-    }
+
 }
 
 function deathHook(a,v) {
     if(wall.length!=0) {
         for(var k=0;k<wall.length;k++) {
             if(v==wall[k][0]) {
-                wall.splice(k,1)
+                wall.splice(k,1);
+                ModPE.showTipMessage(ChatColor.DARK_RED+"Your Wall has been destroyed !")
             }
         }
     }
